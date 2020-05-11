4c948294deefeabcf2e2286bd4547be132b29e8b
pc_liuxun <1193127296@qq.com>
2020-05-12 00:01:48 +0800

    定位完成

--- a.py
+++ a.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import random
 from os import listdir
 from matplotlib import pyplot as plt
+from scipy import ndimage
+from cv2.cv2 import morphologyEx, MORPH_CLOSE, MORPH_OPEN, MORPH_TOPHAT, dilate
 def cv_show(name,img):
     cv.imshow(name, img)
     cv.waitKey(0)
@@ -65,56 +67,176 @@ def cutimg(img_value,ROI_w,ROI_h,ROI_x,ROI_y,type):#裁剪图片
 #         cv_show('x', x)                 
         img.append(x)
     return img
-def tool1(type,imgout,kernel,light_num,thresholdvalue):#卡号定位处理
-    num=t=0
-    ROI_w=ROI_h=ROI_x=ROI_y=0
-    if type==1:
-        retval, dst=cv.threshold(imgout,thresholdvalue+light_num,255,cv.THRESH_BINARY)
-    elif type==2:
-        retval, dst=cv.threshold(imgout,thresholdvalue-15,255,cv.THRESH_BINARY)
-    elif type==3:
-        retval, dst=cv.threshold(imgout,thresholdvalue-light_num-30,255,cv.THRESH_BINARY_INV)
-    dst = cv.morphologyEx(dst,cv.MORPH_GRADIENT,kernel)
-    cv.imshow('tool',dst)
-#     contours, hierarchy=cv.findContours(dst,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-    _,contours, hierarchy=cv.findContours(dst,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-    lendstcon=len(contours)
+def scan(image):
+    def order_points(pts):
+        # 一共4个坐标点
+        rect = np.zeros((4, 2), dtype = "float32")
     
-    for i in range(0,lendstcon):   
-        x, y, w, h = cv.boundingRect(contours[i])
-        
-        # print('ROI:',ROI_x,ROI_y,ROI_w,ROI_h,"\n")
-        if w>150 and h>120 and y>300:
-            ROI_y=0
-            num=0
-            t=10
-            continue
-        # print(x,y,w,h,"\n")
-        if y+h <= 480*0.75-t and y>=200 and h<=46:
-            if ROI_y==0:  
-                ROI_h=46
-                ROI_y=y-(60-h)
-                ROI_x=x 
-                ROI_w=w
-            elif y>=ROI_y and y+h<=ROI_y+46:
-                    if x>ROI_x:
-                        if x>ROI_x+ROI_w:
-                            ROI_w=x-ROI_x+w
-                    else:
-                        ROI_w+=ROI_x-x
-                        ROI_x=x
-                    num+=(ROI_h/20+1)*(ROI_w/30+1)
-            elif ROI_w/640>0.7 and num>20:
-                break
-            else :
-                ROI_h=46
-                ROI_y=y-(46-h)
-                ROI_x=x
-                ROI_w=w
-                num=0
+        # 按顺序找到对应坐标0123分别是 左上，右上，右下，左下
+        # 计算左上，右下
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+    
+        # 计算右上和左下
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect
+
+    def four_point_transform(image, pts):
+        # 获取输入坐标点
+        rect = order_points(pts)
+        (tl, tr, br, bl) = rect
+    
+        # 计算输入的w和h值
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+    
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+    
+        # 变换后对应坐标位置
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+    
+        # 计算变换矩阵
+        M = cv.getPerspectiveTransform(rect, dst)
+        warped = cv.warpPerspective(image, M, (maxWidth, maxHeight))
+        # 返回变换后结果
+        return warped
+        #坐标也会相同变化
+    image = cv.resize(image, (680,500), interpolation=cv.INTER_AREA)
+    orig = image.copy()
+    # 预处理
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (5, 5), 0)
+    edged = cv.Canny(gray, 70, 100)
+    
+    # 展示预处理结果
+    print("STEP 1: 边缘检测")
+    cv.imshow("Image", image)
+    cv.imshow("Edged", edged)
+    
+    # 轮廓检测
+    cnts = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[1]
+    cv.drawContours(image, cnts, -1, (0, 255, 0), 2)
+    cv.imshow('imagecon',image)
+    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:5]
+    
+    # 遍历轮廓
+    for c in cnts:
+        # 计算轮廓近似
+        peri = cv.arcLength(c, True)
+        # C表示输入的点集
+        # epsilon表示从原始轮廓到近似轮廓的最大距离，它是一个准确度参数
+        # True表示封闭的
+        approx = cv.approxPolyDP(c, 0.01 * peri, True)
+    
+        # 4个点的时候就拿出来
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    
+    # 展示结果
+    
+    print("STEP 2: 获取轮廓")
+    cv.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+    cv.imshow("Outline", image)
+    
+    # 透视变换
+    warped = four_point_transform(orig, screenCnt.reshape(4,2))
+    cv.imshow('warped',warped)
+    
+    return warped
+    
+def CardNumLocal(orimg):
+    # 添加矩形框元素
+    def add_cont(x, y, w ,h):
+        p = []
+        p.append(x)
+        p.append(y)
+        p.append(w)
+        p.append(h)
+        return p
+    # 起泡法排序返回最大or最小值
+    def bubble_sort(a, w, s):
+        '''w: 要取的x,y,w,h元素，对应0，1，2，3'''
+        '''s: 0取最小值， 1取最大值'''
+        b = []
+        temp = 0
+        for i in range(len(a)):
+            b.append(a[i][w])
+        b.sort()
+        if s:
+            return b[len(b)-1]
         else:
-            continue
-    return ROI_w,ROI_h,ROI_x,ROI_y,num
+            return b[0]
+    
+    tent = 1
+    startx = 0
+    finalx = 0
+    finaly = 0
+    finalw = 0
+    finalh = 0
+    point = []
+    target = []
+    img=orimg.copy()
+    cv.imshow('img',img)
+    kernel = np.ones((5, 5), np.uint8)
+    newimg = cv.Canny(img, 70, 100)
+    cv.imshow('newimg',newimg)
+    dst1=morphologyEx(newimg,MORPH_CLOSE,kernel)
+    cv.imshow('dst1', dst1)
+    kernel2 = np.ones((3, 3), np.uint8)
+    dst2=morphologyEx(dst1,MORPH_OPEN,kernel2)
+    cv.imshow('dst2', dst2)
+    image_contours, contours, hier = cv.findContours(dst2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for i in range(len(contours)):
+        cnt = contours[i]
+        x, y, w, h = cv.boundingRect(cnt)
+        if 40>w>15 and 50>h>25:
+            point.append(add_cont(x,y,w,h))
+            cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+    cv.imshow('imggg',img)
+    imggg1=orimg.copy()
+            
+    for o in range(len(point)):
+        for i in range(len(point)):
+            if i != o:
+                xx= abs(point[o][1] - point[i][1])
+                if xx>=0 and xx<=10:
+                    tent += 1
+                    
+                if  tent >10 :
+                    tent = 1
+                    target.append(point[o])
+                    x,y,w,h=point[o][0],point[o][1],point[o][2],point[o][3]
+                    cv.rectangle(imggg1,(x,y),(x+w,y+h),(255,0,0),2)
+                    break
+    cv.imshow('imggg1',imggg1)          
+    newimg[0:target[0][1]-3,0:800] = 0
+    newimg[target[0][1]+target[0][3]+3:400, 0:800] = 0
+    cv.imshow('newimg2',newimg)
+    finalx = bubble_sort(point, 0, 1)
+    startx = bubble_sort(point, 0, 0) - 3
+    starty = bubble_sort(point, 1, 0) - 3
+    finalx = finalx + bubble_sort(point, 2, 1)
+    finaly = starty + bubble_sort(point, 3, 1) + 10
+    print("startx = "+ str(startx))
+    print("starty = "+ str(finaly))
+    print("width = "+ str(finalw))
+    print("height = "+ str(finalh))
+    # 画出矩形框
+    cv.rectangle(img,(startx,starty),(finalx, finaly),(0, 255, 0),2)
+    cv.imshow("org", img)
+    return finalx-startx,finaly-starty,startx,starty
+    
 def getlight(grayimg):
     light=0
     cout=0   
@@ -125,54 +247,14 @@ def getlight(grayimg):
     imglight=light//cout
     return imglight
 def imghandle(img_name):#图片处理
-    img = cv.imread(img_name)
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    img=cv.resize(img,(640,480))#准备参数
-    imgout = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    cv.imshow('imgout',imgout)
-    thresholdvalue=getlight(imgout)
-    light_num=30
-    kernel = np.ones((9,9), np.uint8)
-#     print(cv.__version__,thresholdvalue)
-    if thresholdvalue>190:
-        light_num=50
-    elif thresholdvalue>200:
-        light_num=100
-    dst = cv.morphologyEx(imgout,cv.MORPH_TOPHAT,kernel)#形态处理
-    cv.imshow('dis1',dst)
-    gradX = cv.Sobel(dst,cv.CV_32F,1,0,-1)
-    gradX = np.absolute(gradX)
-    gradX = (255 * ((gradX -np.min(gradX)) / (np.max(gradX) -np.min(gradX))))  
-    dst = gradX.astype("uint8")#梯度处理
-    dst = cv.morphologyEx(dst,cv.MORPH_CLOSE,kernel)
-    retval, dst=cv.threshold(imgout,thresholdvalue+light_num,255,cv.THRESH_BINARY)
-    cv.imshow('dis2', dst)
-    ROI_w,ROI_h,ROI_x,ROI_y,num=tool1(1,imgout,kernel,light_num,thresholdvalue)#卡号定位处理
-    if ROI_w/640>0.7 and num>20:
-        ROI_w+=abs(640-ROI_w-ROI_x)
-        ROI_x-=10
-        cv.imshow("imggg",img)
-        handle=cutimg(img,ROI_w,ROI_h,ROI_x,ROI_y,1)
-        cv.rectangle(img, (ROI_x,ROI_y), (ROI_x+ROI_w,ROI_y+ROI_h), (255,0,0), 2)
-        plt.imshow(img)
-        plt.show()
-        return handle
-    ROI_w,ROI_h,ROI_x,ROI_y,num=tool1(2,imgout,kernel,light_num,thresholdvalue)
-    if ROI_w/640>0.7 and num>20:
-        ROI_w+=abs(640-ROI_w-ROI_x)
-        ROI_x-=10
-        handle=cutimg(img,ROI_w,ROI_h,ROI_x,ROI_y,2)
-        cv.rectangle(img, (ROI_x,ROI_y), (ROI_x+ROI_w,ROI_y+ROI_h), (165,165,255), 2)
-        plt.imshow(img)
-        plt.show()
-        return handle
-    ROI_w,ROI_h,ROI_x,ROI_y,num=tool1(3,imgout,kernel,light_num,thresholdvalue)
-    if ROI_w/640>0.7 and num>20:
-        ROI_w+=abs(640-ROI_w-ROI_x)
-        ROI_x-=10
-        handle=cutimg(img,ROI_w,ROI_h,ROI_x,ROI_y,3)
-        cv.rectangle(img, (ROI_x,ROI_y), (ROI_x+ROI_w,ROI_y+ROI_h), (165,165,255), 2)
-        plt.imshow(img)
-        plt.show()
-        return handle#返回裁剪好的数字列表
-    return 0,0,0,0
+    handle=[]
+    orgimg = cv.imread(img_name)
+    imgout=scan(orgimg)
+    imgout = cv.resize(imgout, (800,400), interpolation=cv.INTER_AREA)
+    cv.imshow('imgout1',imgout)
+    ROI_w,ROI_h,ROI_x,ROI_y=CardNumLocal(imgout)#卡号定位处理
+    cv.imshow('imgout2',imgout)
+    cv.rectangle(imgout, (ROI_x,ROI_y), (ROI_x+ROI_w,ROI_y+ROI_h), (255,0,0), 2)
+    plt.imshow(imgout)
+    plt.show()
+    return handle   
